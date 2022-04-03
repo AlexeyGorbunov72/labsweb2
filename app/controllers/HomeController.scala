a222f1aaf27ba1b104500c02d7e309113209d341
Aleksei Gorbunov <alekseigor@yandex-team.ru>
2022-04-03 18:14:34 +0300

    Лаба 2

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,12 +1,32 @@
 package controllers
-
+import play.api.Logger
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import rx.lang.scala._
+import services.ProfileService
 
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(
+  val controllerComponents: ControllerComponents,
+  val navigationController: NavigationController,
+  val profileService: ProfileService
+  ) extends BaseController {
+
+  private var disposable: Option[Subscription] = Option(null)
+
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.home.view())
+    Logger("access").debug("home")
+    println("ijodijiwofjwieo")
+    if (this.disposable.isDefined) {
+      this.disposable.get.unsubscribe()
+    }
+    this.disposable = Some(
+      profileService.isUserSignInObservable.subscribe({ _ =>
+        routes.HomeController.index()
+      })
+     )
+    val nav = navigationController.makeView()
+    Ok(views.html.home.view(nav))
   }
 }
